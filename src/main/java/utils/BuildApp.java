69c7ff04921df22a01e27856a8dd5705b569e6a3
JuanDelahozMiranda <juan.delahozdevelop@gmail.com>
2021-09-30 03:09:43 -0500

    Update BuildApp.java

--- src/main/java/utils/BuildApp.java
+++ src/main/java/utils/BuildApp.java
@@ -10,8 +10,8 @@ public class BuildApp {
 
     public AppiumDriver<MobileElement> setUpAppium() throws MalformedURLException {
         DesiredCapabilities capa = new DesiredCapabilities();
-            capa.setCapability("deviceName", "SM-A305G");
-            capa.setCapability("udid", "R58M72AMYWT");
+            capa.setCapability("deviceName", "");
+            capa.setCapability("udid", "");
             capa.setCapability("platformName", "Android");
             capa.setCapability("platformVersion", "10");
             capa.setCapability("appPackage", "com.ba.universalconverter");
