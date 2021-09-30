af3ef06005cdbfeafaa889f5391ec808418162ab
JuanDelahozMiranda <juan.delahozdevelop@gmail.com>
2021-09-30 02:25:30 -0500

    Update readme file

--- src/main/java/interfaces/homeConverter.java
+++ src/main/java/interfaces/homeConverter.java
@@ -126,7 +126,7 @@ public class homeConverter {
     }
 
     public void pressVelocidadOption(AppiumDriver<MobileElement> driver){
-        MobileElement OpVelocidad = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.RelativeLayout/android.widget.ListView/android.widget.RelativeLayout[2]"));
+        MobileElement OpVelocidad = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.RelativeLayout/android.widget.ListView/android.widget.RelativeLayout[4]"));
             OpVelocidad.click();
     }
 
